c78b6c17f9d564c3ce8f32858a65caa82a49bbc5
Josh <Josh@elementor.com>
2020-03-24 15:51:49 +0200

    Improve masonry logic for more balanced layout

--- src/js/types/masonry.js
+++ src/js/types/masonry.js
@@ -11,49 +11,56 @@ export default class Masonry extends BaseGalleryType {
 		this.currentBreakpoint = currentBreakpoint;
 
 		const heights = [],
+			itemsInColumn = [],
 			aggregatedHeights = [],
 			columns = this.getCurrentDeviceSetting( 'columns' ),
 			containerWidth = this.$container.width(),
 			horizontalGap = this.getCurrentDeviceSetting( 'horizontalGap' ),
 			itemWidth = ( containerWidth - ( horizontalGap * ( columns - 1 ) ) ) / columns,
 			$items = this.getActiveItems();
+		let columnHeight = 0;
+
+		for ( let i = 0; i < columns; i++ ) {
+			itemsInColumn[ i ] = 0;
+			heights[ i ] = 0;
+		}
 
 		$items.each( ( index, item ) => {
-			const row = Math.floor( index / columns ),
-				indexAtRow = index % columns,
-				imageData = this.getImageData( index ),
+			const imageData = this.getImageData( index ),
 				itemHeight = itemWidth / imageData.ratio;
+			let indexAtRow = index % columns;
+
+			columnHeight = heights[ indexAtRow ];
+			jQuery.each( heights, ( colNumber, colHeight ) => {
+				if ( ( colHeight > 5 ) && columnHeight > colHeight + 5 ) {
+					columnHeight = colHeight;
+					indexAtRow = colNumber;
+				}
+			} );
+
+			aggregatedHeights[ index ] = heights[ indexAtRow ];
+			heights[ indexAtRow ] += itemHeight;
 
 			item.style.setProperty( '--item-height', ( imageData.height / imageData.width * 100 ) + '%' );
 			item.style.setProperty( '--column', indexAtRow );
-
-			if ( row ) {
-				aggregatedHeights[ index ] = heights[ indexAtRow ];
-
-				heights[ indexAtRow ] += itemHeight;
-			} else {
-				heights.push( itemHeight );
-			}
+			item.style.setProperty( '--items-in-column', itemsInColumn[ indexAtRow ] );
+			itemsInColumn[ indexAtRow ]++;
 		} );
 
 		const highestColumn = Math.max( ...heights ),
 			highestColumnIndex = heights.indexOf( highestColumn ),
-			rows = Math.floor( this.settings.items.length / columns ),
-			rowsRemainder = this.settings.items.length % columns,
-			highestColumnsGapsCount = rowsRemainder > highestColumnIndex ? rows : rows - 1,
+			rows = itemsInColumn[ highestColumnIndex ],
+			highestColumnsGapsCount = rows - 1,
 			containerAspectRatio = highestColumn / containerWidth;
 
 		this.$container[ 0 ].style.setProperty( '--columns', columns );
-		this.$container[ 0 ].style.setProperty( '--highest-column-gap-count', highestColumnsGapsCount );
+		this.$container[ 0 ].style.setProperty( '--highest-column-gap-count', highestColumnsGapsCount.toString() );
 
 		this.$container.css( 'padding-bottom', ( containerAspectRatio * 100 ) + '%' );
 
 		$items.each( ( index, item ) => {
-			const percentHeight = aggregatedHeights[ index ] ? aggregatedHeights[ index ] / highestColumn * 100 : 0,
-				row = Math.floor( index / columns );
-
+			const percentHeight = aggregatedHeights[ index ] ? aggregatedHeights[ index ] / highestColumn * 100 : 0;
 			item.style.setProperty( '--percent-height', percentHeight + '%' );
-			item.style.setProperty( '--row', row );
 		} );
 	}
 }
